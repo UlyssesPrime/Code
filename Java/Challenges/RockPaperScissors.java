dc4d2e4eab5317dc6dd463dd56cb45b7209fb4e6
Jack Walsh <jack99walsh@gmail.com>
2017-09-29 09:29:20 -0400

    RockPaperScissors.java works and is compiled

--- Java/Challenges/RockPaperScissors.java
+++ Java/Challenges/RockPaperScissors.java
@@ -1,3 +1,30 @@
-public class RockPaperScissors{
-
+import java.util.Scanner;
+public class RockPaperScissors {
+  public static void main(String[] args){
+    System.out.println("What will you throw? Your options are Rock, Paper, Scissors, Lizard, and Spock.");
+    Scanner in = new Scanner(System.in);
+    String pThrow = in.nextLine();
+    String cThrow = " ";
+    double gThrow = 0.0;
+    if (gThrow >= 0 && gThrow < 0.2){
+      cThrow = "Rock";
+    }
+    else if (gThrow >= 0.2 && gThrow < 0.4){
+      cThrow = "Paper";
+    }
+    else if (gThrow >= 0.4 && gThrow < 0.6){
+      cThrow = "Scissors";
+    }
+    else if (gThrow >= 0.6 && gThrow < 0.8){
+      cThrow = "Lizard";
+    }
+    else if (gThrow >= 0.8 && gThrow < 1){
+      cThrow = "Spock";
+    }
+    else {
+      System.out.print("BUG ALERT");
+      cThrow = "ERROR";
+    }
+    System.out.println("The computer threw: " + cThrow);
+  }
 }
