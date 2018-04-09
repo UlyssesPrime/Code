1a45a03338feb3387feb452d29a0e46bb647a2b7
Jack Walsh <jack99walsh@gmail.com>
2018-04-09 13:54:23 -0500

    Removed that gay shit with spock and lizards

--- Java/Challenges/RockPaperScissors.java
+++ Java/Challenges/RockPaperScissors.java
@@ -1,30 +1,50 @@
 import java.util.Scanner;
 public class RockPaperScissors {
   public static void main(String[] args){
-    System.out.println("What will you throw? Your options are Rock, Paper, Scissors, Lizard, and Spock.");
+    System.out.println("What will you throw? Your options are Rock, Paper, or Scissors.");
     Scanner in = new Scanner(System.in);
     String pThrow = in.nextLine();
     String cThrow = " ";
-    double gThrow = 0.0;
-    if (gThrow >= 0 && gThrow < 0.2){
+    double gThrow = Math.random();
+    if (gThrow >= 0 && gThrow < 0.33){
       cThrow = "Rock";
     }
-    else if (gThrow >= 0.2 && gThrow < 0.4){
+    else if (gThrow >= 0.34 && gThrow < 0.66){
       cThrow = "Paper";
     }
-    else if (gThrow >= 0.4 && gThrow < 0.6){
+    else if (gThrow >= 0.67 && gThrow < 1){
       cThrow = "Scissors";
-    }
-    else if (gThrow >= 0.6 && gThrow < 0.8){
-      cThrow = "Lizard";
-    }
-    else if (gThrow >= 0.8 && gThrow < 1){
-      cThrow = "Spock";
     }
     else {
       System.out.print("BUG ALERT");
       cThrow = "ERROR";
     }
+    String winLose;
+    if (pThrow.toLowerCase().equals(cThrow.toLowerCase())){
+      winLose = "You tied!";
+    }
+    else if(pThrow.toLowerCase().equals("rock") && cThrow.equals("Paper")){
+      winLose = "You lose!";
+    }
+    else if(pThrow.toLowerCase().equals("paper") && cThrow.equals("Scissors")){
+      winLose = "You lose!";
+    }
+    else if(pThrow.toLowerCase().equals("scissors") && cThrow.equals("Rock")){
+      winLose = "You lose!";
+    }
+    else if(pThrow.toLowerCase().equals("rock") && cThrow.equals("Scissors")){
+      winLose = "You win!";
+    }
+    else if(pThrow.toLowerCase().equals("paper") && cThrow.equals("Rock")){
+      winLose = "You win!";
+    }
+    else if(pThrow.toLowerCase().equals("scissors") && cThrow.equals("Paper")){
+      winLose = "You win!";
+    }
+    else{
+      winLose = "ERROR!";
+    }
     System.out.println("The computer threw: " + cThrow);
+    System.out.println(winLose);
   }
 }
